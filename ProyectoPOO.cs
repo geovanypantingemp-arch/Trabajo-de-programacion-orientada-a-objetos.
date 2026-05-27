f083d8bcc36ee845e14cb852db16a62ca3425d2a
geovanypantingemp-arch <geovanypantingemp@gmail.com>
2026-05-27 07:30:36 -0600

    Add Producto class with properties and constructor

--- ProyectoPOO.cs
+++ ProyectoPOO.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Colections.Generic;
+namespace ElTrueque
+{
+  class Producto
+  {
+    public string Nombre {get; set;}
+    public string Productora {get, set;}
+    public int Cantidad {get; set;}
+    public Producto(string nombre, string productora, int cantidad)
+    {
+      Nombre = nombre;
+      Productora = productora;
+      Cantidad = cantidad;
+    }
+  }
+      
+
+    
 
